5804c2a985894f23914356bceb97461600587e0b
Pramish-Aryal <076bct046.pramish@pcamus.edu,np>
2020-06-29 09:12:23 +0545

    hmm nice refactoring

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('button');
 const gameHeading = document.querySelector('#gameHeading');
-
 //Play again button
 const playAgain = document.createElement('button');
 playAgain.classList.add('playAgain');
@@ -8,59 +7,36 @@ playAgain.innerText = 'Play Again?';
 playAgain.addEventListener('click', () => {
 	location.reload();
 });
-
 //Sounds
-let redSound = new Audio('sounds/red.mp3');
-let greenSound = new Audio('sounds/green.mp3');
-let blueSound = new Audio('sounds/blue.mp3');
-let yellowSound = new Audio('sounds/yellow.mp3');
-let wrong = new Audio('sounds/wrong.mp3');
-
-let level = 0;
-
-let sounds = [ redSound, greenSound, blueSound, yellowSound, wrong ];
-//RGBY 0123
+let sounds = [new Audio('sounds/red.mp3'), new Audio('sounds/green.mp3'), new Audio('sounds/blue.mp3'), new Audio('sounds/yellow.mp3'), new Audio('sounds/wrong.mp3')];
+//RGBYW 01234
 const red = 0;
 const green = 1;
 const blue = 2;
 const yellow = 3;
-
+const wrong = 4;
+//Game variables
 let gameArray = [];
 let userSelected = [];
-
 let gameRunning = false;
+let level = 1;
+const timeDelay = 500;
+//Function declerations
+function checker() {
+	for (let i = 0; i < userSelected.length; i++) {
+		if (gameArray[i] !== userSelected[i]) return false;
+	}
+	return true;
+}
 
 function over() {
 	gameHeading.innerHTML = `<h1>Game Over</h1><h6>You scored: ${level}</h6>`;
 	document.body.append(playAgain);
 }
 
-const timeDelay = 500;
-function listener(event, color) {
-	if (gameRunning) {
-		userSelected.push(color);
-		if (!checker()) {
-			wrong.play();
-			over();
-			return;
-		}
-		clickedFlash(color);
-		if (userSelected.length === level) setTimeout(levelLogic, timeDelay);
-	}
-}
-
-buttons[red].addEventListener('click', listener.bind(null, event, red));
-
-buttons[green].addEventListener('click', listener.bind(null, event, green));
-
-buttons[blue].addEventListener('click', listener.bind(null, event, blue));
-
-buttons[yellow].addEventListener('click', listener.bind(null, event, yellow));
-
 function levelLogic() {
 	if (!gameRunning) {
 		gameRunning = true;
-		level = 1;
 	} else {
 		userSelected = [];
 		level++;
@@ -81,9 +57,19 @@ function clickedFlash(color) {
 	sounds[color].play();
 }
 
-function checker() {
-	for (let i = 0; i < userSelected.length; i++) {
-		if (gameArray[i] !== userSelected[i]) return false;
+function listener(event, color) {
+	if (gameRunning) {
+		userSelected.push(color);
+		if (!checker()) {
+			sounds[wrong].play();
+			over();
+			return;
+		}
+		clickedFlash(color);
+		if (userSelected.length === level) setTimeout(levelLogic, timeDelay);
 	}
-	return true;
 }
+buttons[red].addEventListener('click', listener.bind(null, event, red));
+buttons[green].addEventListener('click', listener.bind(null, event, green));
+buttons[blue].addEventListener('click', listener.bind(null, event, blue));
+buttons[yellow].addEventListener('click', listener.bind(null, event, yellow));
